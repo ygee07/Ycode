a23428c212769e5d12a634be1aa1b264c6b51690
greyder <ygee@yearz.co>
2023-01-26 19:34:03 +0800

    Initial Commit

---
+++ Ycode/YcodeApp.swift
@@ -0,0 +1,17 @@
+//
+//  YcodeApp.swift
+//  Ycode
+//
+//  Created by Yhanco Grey Esteban on 1/26/23.
+//
+
+import SwiftUI
+
+@main
+struct YcodeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
